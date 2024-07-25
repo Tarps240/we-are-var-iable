ddac69d7397d8306c37a57b4138d9f8c6e3ae034
Tarps240 <sl3966@gmail.com>
2024-07-25 06:21:38 -0500

    finished homework

---
+++ main.js
@@ -0,0 +1,84 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+
+let myFirstName = 'Scott';
+let myLastName = 'Lewis';
+let myFavoriteCeleb = 'RDJ';
+let mySentence = 'Hi! My name is' + ' ' + myFirstName + ' ' + myLastName + ' ' + 'and my favorite celebrity is' + ' ' + myFavoriteCeleb + '.';
+let myLuckyNum = 7;
+let myAge = 43;
+let myHeight = 74;
+let myFavoriteYear = 2022;
+
+/* Math Problems */
+
+mySum = 5 + 2317 + 36;
+myProduct = 232 * 43 / 2 + 4;
+myHeightAndAge = myHeight + myAge;
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
